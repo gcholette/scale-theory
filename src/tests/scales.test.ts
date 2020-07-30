0cef054cf8e9a4574667183b0c9ad39732d85492
gcholette <solutions@gcholette.com>
2020-07-29 20:49:58 -0400

    rename tests

--- src/tests/scales.test.ts
+++ src/tests/scales.test.ts
@@ -1,42 +1,42 @@
 import {modes} from '../constants'
 import {getModeSemitones, getScale, getModeSteps} from '../scales'
 
-test('getModeSteps ionian should return "whole whole half whole whole whole half"', () => {
+test('getModeSteps ionian', () => {
     const steps = getModeSteps('ionian')
     expect(steps).toStrictEqual([2, 2, 1, 2, 2, 2, 1])
 })
 
-test('getModeSteps aeolian should return "whole half whole whole half whole whole"', () => {
+test('getModeSteps aeolian', () => {
     const steps = getModeSteps('aeolian')
     expect(steps).toMatchObject([2, 1, 2, 2, 1, 2, 2])
 })
 
-test('getModeSteps mixolodian should return "whole whole half whole whole half whole"', () => {
+test('getModeSteps mixolodian', () => {
     const steps = getModeSteps('mixolodian')
     expect(steps).toMatchObject([2, 2, 1, 2, 2, 1, 2])
 })
 
-test('getModeSemitones C Major should return 0 2 4 5 7 9 11 0', () => {
+test('getModeSemitones C Major', () => {
     const semitones = getModeSemitones('ionian', 'C')
     expect(semitones).toMatchObject([0, 2, 4, 5, 7, 9, 11, 0])
 })
 
-test('getModeSemitones D phrygian 2 3 5 7 9 10 0 2', () => {
+test('getModeSemitones D phrygian', () => {
     const semitones = getModeSemitones('phrygian', 'D')
     expect(semitones).toMatchObject([2, 3, 5, 7, 9, 10, 0, 2])
 })
 
-test('getModeSemitones B Lydian 2 3 5 7 9 10 0 2', () => {
+test('getModeSemitones B Lydian', () => {
     const semitones = getModeSemitones('phrygian', 'D')
     expect(semitones).toMatchObject([2, 3, 5, 7, 9, 10, 0, 2])
 })
 
-test('C Major scale generation', () => {
+test('getScale C Major', () => {
     const scale = getScale('ionian', 'C')
     expect(scale).toStrictEqual(['C', 'D', 'E', 'F', 'G', 'A', 'B', 'C'])
 })
 
-test('A Natural minor scale generation', () => {
+test('getScale A Natural minor', () => {
     const scale = getScale('minor', 'A')
     expect(scale).toStrictEqual(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'A'])
 })
